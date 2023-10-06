484174abe42d8a2752504e66e83e30df9d8dc32e
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-10-06 17:46:47 -0300

    invert triggers run order

--- terraform/meta/tfe-run-trigger.tf
+++ terraform/meta/tfe-run-trigger.tf
@@ -1,45 +1,45 @@
 # only 1 trigger exists, this or manually entered
 resource "tfe_run_trigger" "meta_seeds" {
-  workspace_id  = data.tfe_workspace.meta.id
-  sourceable_id = tfe_workspace.seeds.id
+  workspace_id  = tfe_workspace.seeds.id
+  sourceable_id = data.tfe_workspace.meta.id
 }
 
 resource "tfe_run_trigger" "seeds_vpc" {
-  workspace_id  = tfe_workspace.seeds.id
-  sourceable_id = tfe_workspace.vpc.id
+  workspace_id  = tfe_workspace.vpc.id
+  sourceable_id = tfe_workspace.seeds.id
 }
 
 resource "tfe_run_trigger" "vpc_buckets" {
-  workspace_id  = tfe_workspace.vpc.id
-  sourceable_id = tfe_workspace.buckets.id
+  workspace_id  = tfe_workspace.buckets.id
+  sourceable_id = tfe_workspace.vpc.id
 }
 
 resource "tfe_run_trigger" "buckets_databases" {
-  workspace_id  = tfe_workspace.buckets.id
-  sourceable_id = tfe_workspace.databases.id
+  workspace_id  = tfe_workspace.databases.id
+  sourceable_id = tfe_workspace.buckets.id
 }
 
 resource "tfe_run_trigger" "databases_secrets" {
-  workspace_id  = tfe_workspace.databases.id
-  sourceable_id = tfe_workspace.secrets.id
+  workspace_id  = tfe_workspace.secrets.id
+  sourceable_id = tfe_workspace.databases.id
 }
 
 resource "tfe_run_trigger" "secrets_k8s" {
-  workspace_id  = tfe_workspace.secrets.id
-  sourceable_id = tfe_workspace.k8s.id
+  workspace_id  = tfe_workspace.k8s.id
+  sourceable_id = tfe_workspace.secrets.id
 }
 
 resource "tfe_run_trigger" "secrets_apps" {
-  workspace_id  = tfe_workspace.apps.id
-  sourceable_id = tfe_workspace.k8s.id
+  workspace_id  = tfe_workspace.k8s.id
+  sourceable_id = tfe_workspace.apps.id
 }
 
 resource "tfe_run_trigger" "k8s_dns" {
-  workspace_id  = tfe_workspace.k8s.id
-  sourceable_id = tfe_workspace.dns.id
+  workspace_id  = tfe_workspace.dns.id
+  sourceable_id = tfe_workspace.k8s.id
 }
 
 resource "tfe_run_trigger" "apps_dns" {
-  workspace_id  = tfe_workspace.apps.id
-  sourceable_id = tfe_workspace.dns.id
+  workspace_id  = tfe_workspace.dns.id
+  sourceable_id = tfe_workspace.apps.id
 }
