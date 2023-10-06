44ff969de42eee38f58a2b208e45f492af9c401b
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-10-06 15:40:31 -0300

    update in env variables to workspaces

--- terraform/meta/tfe-variables-dns.tf
+++ terraform/meta/tfe-variables-dns.tf
@@ -1,6 +1,21 @@
+resource "tfe_variable" "dns_cloudflare_account_id" {
+  key          = "account_id"
+  value        = var.cloudflare_account_id
+  category     = "terraform"
+  workspace_id = tfe_workspace.dns.id
+}
+
+resource "tfe_variable" "dns_cloudflare_api_token" {
+  key          = "CLOUDFLARE_API_TOKEN"
+  value        = var.cloudflare_api_token
+  category     = "env"
+  workspace_id = tfe_workspace.dns.id
+}
+
 resource "tfe_variable" "dns_domain" {
   key          = "domain"
   value        = var.domain
   category     = "terraform"
   workspace_id = tfe_workspace.dns.id
 }
+
