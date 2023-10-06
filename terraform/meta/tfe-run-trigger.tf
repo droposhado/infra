7f2c2293a648828a2c84f8cda6068efcf15ea9eb
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-10-05 23:45:01 -0300

    test

--- terraform/meta/tfe-run-trigger.tf
+++ terraform/meta/tfe-run-trigger.tf
@@ -1,7 +1,7 @@
 # only 1 trigger exists, this or manually entered
 resource "tfe_run_trigger" "meta_seeds" {
-  workspace_id  = tfe_workspace.seeds.id
-  sourceable_id = data.tfe_workspace.meta.id
+  workspace_id  = data.tfe_workspace.meta.id
+  sourceable_id = tfe_workspace.seeds.id
 }
 
 resource "tfe_run_trigger" "seeds_vpc" {
