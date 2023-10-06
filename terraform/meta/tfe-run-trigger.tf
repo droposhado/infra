45c821c9cb6e75c976206cb77934fe0aca4e7cae
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-10-05 23:52:03 -0300

    fix trigger runs and seed secret key

--- terraform/meta/tfe-run-trigger.tf
+++ terraform/meta/tfe-run-trigger.tf
@@ -5,41 +5,41 @@ resource "tfe_run_trigger" "meta_seeds" {
 }
 
 resource "tfe_run_trigger" "seeds_vpc" {
-  workspace_id  = tfe_workspace.vpc.id
-  sourceable_id = tfe_workspace.seeds.id
-}
-
-resource "tfe_run_trigger" "vpc_buckets" {
-  workspace_id  = tfe_workspace.buckets.id
+  workspace_id  = tfe_workspace.seeds.id
   sourceable_id = tfe_workspace.vpc.id
 }
 
-resource "tfe_run_trigger" "buckets_databases" {
-  workspace_id  = tfe_workspace.databases.id
+resource "tfe_run_trigger" "vpc_buckets" {
+  workspace_id  = tfe_workspace.vpc.id
   sourceable_id = tfe_workspace.buckets.id
 }
 
-resource "tfe_run_trigger" "databases_secrets" {
-  workspace_id  = tfe_workspace.secrets.id
+resource "tfe_run_trigger" "buckets_databases" {
+  workspace_id  = tfe_workspace.buckets.id
   sourceable_id = tfe_workspace.databases.id
 }
 
-resource "tfe_run_trigger" "secrets_k8s" {
-  workspace_id  = tfe_workspace.k8s.id
+resource "tfe_run_trigger" "databases_secrets" {
+  workspace_id  = tfe_workspace.databases.id
   sourceable_id = tfe_workspace.secrets.id
 }
 
-resource "tfe_run_trigger" "secrets_apps" {
-  workspace_id  = tfe_workspace.k8s.id
-  sourceable_id = tfe_workspace.apps.id
-}
-
-resource "tfe_run_trigger" "k8s_dns" {
-  workspace_id  = tfe_workspace.dns.id
+resource "tfe_run_trigger" "secrets_k8s" {
+  workspace_id  = tfe_workspace.secrets.id
   sourceable_id = tfe_workspace.k8s.id
 }
 
+resource "tfe_run_trigger" "secrets_apps" {
+  workspace_id  = tfe_workspace.apps.id
+  sourceable_id = tfe_workspace.k8s.id
+}
+
+resource "tfe_run_trigger" "k8s_dns" {
+  workspace_id  = tfe_workspace.k8s.id
+  sourceable_id = tfe_workspace.dns.id
+}
+
 resource "tfe_run_trigger" "apps_dns" {
-  workspace_id  = tfe_workspace.dns.id
-  sourceable_id = tfe_workspace.apps.id
+  workspace_id  = tfe_workspace.apps.id
+  sourceable_id = tfe_workspace.dns.id
 }
