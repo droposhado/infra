321b4292635a005e17a51a4f0a1a85277031d847
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-10-06 17:25:43 -0300

    correct azs definition

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -4,7 +4,7 @@ module "vpc" {
 
   name                 = "cigarra"
   cidr                 = "10.0.0.0/16"
-  azs                  = data.aws_availability_zones.available.names
+  azs                  = local.azs
   public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   enable_dns_hostnames = true
   enable_dns_support   = true
