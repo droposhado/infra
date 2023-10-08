483d325e8db37fb81ef14fab1394336a48bc226f
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-10-08 01:11:51 -0300

    new variables to buckets

--- terraform/meta/tfe-variables-buckets.tf
+++ terraform/meta/tfe-variables-buckets.tf
@@ -1,3 +1,24 @@
+resource "tfe_variable" "buckets_aws_access_key_id" {
+  key          = "AWS_ACCESS_KEY_ID"
+  value        = var.aws_access_key_id
+  category     = "env"
+  workspace_id = tfe_workspace.buckets.id
+}
+
+resource "tfe_variable" "buckets_aws_region_default" {
+  key          = "aws_region_default"
+  value        = var.aws_region_default
+  category     = "terraform"
+  workspace_id = tfe_workspace.buckets.id
+}
+
+resource "tfe_variable" "buckets_aws_secret_access_key" {
+  key          = "AWS_SECRET_ACCESS_KEY"
+  value        = var.aws_secret_access_key
+  category     = "env"
+  workspace_id = tfe_workspace.buckets.id
+}
+
 resource "tfe_variable" "buckets_domain" {
   key          = "domain"
   value        = var.domain
