a9c8739953ea9377499f2f50a15f50fd73ed4571
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-10-08 15:23:32 -0300

    update dns remove uptime kuma

--- terraform/dns/variables.tf
+++ terraform/dns/variables.tf
@@ -2,6 +2,15 @@ variable "account_id" {
   type = string
 }
 
+variable "aws_region_default" {
+  type        = string
+  description = "Default AWS region to create resources"
+}
+
+variable "default_ttl" {
+  type = string
+}
+
 variable "domain" {
   type = string
 }
