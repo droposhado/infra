80a6ecf851c60c3522ee9af66039bdcd06a45002
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-10-08 15:07:20 -0300

    adding aws keys to dns

--- terraform/meta/tfe-variables-dns.tf
+++ terraform/meta/tfe-variables-dns.tf
@@ -1,3 +1,24 @@
+resource "tfe_variable" "dss_aws_access_key_id" {
+  key          = "AWS_ACCESS_KEY_ID"
+  value        = var.aws_access_key_id
+  category     = "env"
+  workspace_id = tfe_workspace.dns.id
+}
+
+resource "tfe_variable" "dns_aws_region_default" {
+  key          = "aws_region_default"
+  value        = var.aws_region_default
+  category     = "terraform"
+  workspace_id = tfe_workspace.dns.id
+}
+
+resource "tfe_variable" "dns_aws_secret_access_key" {
+  key          = "AWS_SECRET_ACCESS_KEY"
+  value        = var.aws_secret_access_key
+  category     = "env"
+  workspace_id = tfe_workspace.dns.id
+}
+
 resource "tfe_variable" "dns_cloudflare_account_id" {
   key          = "account_id"
   value        = var.cloudflare_account_id
