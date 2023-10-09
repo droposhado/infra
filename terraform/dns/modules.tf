533ce9144cacc63ec8e8890931bf0a0676e4ec35
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-10-08 22:30:30 -0300

    module output

--- terraform/dns/modules.tf
+++ terraform/dns/modules.tf
@@ -1,17 +1,13 @@
-data "cloudflare_zone" "droposhado_org" {
-  name = var.domain
-}
-
-module "migadu-email" {
-  source  = "droposhado/migadu-mail/cloudflare"
-  version = "1.0.0"
-  zone_id = data.cloudflare_zone.droposhado_org.id
-  domain  = var.domain
-}
-
 module "cloudflare" {
   source     = "./modules/cloudflare"
   account_id = var.account_id
   domain     = var.domain
   records    = local.cloudflare_records
+}
+
+module "migadu-email" {
+  source  = "droposhado/migadu-mail/cloudflare"
+  version = "1.0.0"
+  zone_id = modules.cloudflare.zone_id
+  domain  = var.domain
 }
