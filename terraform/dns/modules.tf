b5655cb5674bf93ee84564d6db5b15c2953d9a2c
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-10-08 22:52:40 -0300

    module output

--- terraform/dns/modules.tf
+++ terraform/dns/modules.tf
@@ -8,6 +8,6 @@ module "cloudflare" {
 module "migadu-email" {
   source  = "droposhado/migadu-mail/cloudflare"
   version = "1.0.0"
-  zone_id = modules.cloudflare.zone_id
+  zone_id = module.cloudflare.zone_id
   domain  = var.domain
 }
