ebee3506bdf4c31ac6d1ec8cee330f08ea9e2450
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-10-09 13:41:18 -0300

    n/a

--- terraform/dns/modules/cloudflare-ns/cloudflare-records.tf
+++ terraform/dns/modules/cloudflare-ns/cloudflare-records.tf
@@ -1,10 +1,10 @@
-resource "cloudflare_record" "ns" {
-  zone_id  = cloudflare_zone.main.id
-  for_each = toset(var.ns)
+# resource "cloudflare_record" "ns" {
+#   zone_id  = cloudflare_zone.main.id
+#   for_each = { for idx, val in local.ns : idx => val }
 
-  name    = "@"
-  value   = each.value
-  type    = "NS"
-  ttl     = 60
-  proxied = false
-}
+#   name    = "@"
+#   value   = each.value
+#   type    = "NS"
+#   ttl     = 60
+#   proxied = false
+# }
