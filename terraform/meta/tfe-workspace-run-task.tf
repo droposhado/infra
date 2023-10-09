10dd330a1891824dafbb36f2637b7ecea4ba2e1d
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-10-09 12:23:19 -0300

    task trigger from pre_apply to post_plan

--- terraform/meta/tfe-workspace-run-task.tf
+++ terraform/meta/tfe-workspace-run-task.tf
@@ -2,33 +2,33 @@ resource "tfe_workspace_run_task" "network" {
   workspace_id      = tfe_workspace.network.id
   task_id           = tfe_organization_run_task.snyk.id
   enforcement_level = "mandatory"
-  stage             = "pre_apply"
+  stage             = "post_plan"
 }
 
 resource "tfe_workspace_run_task" "buckets" {
   workspace_id      = tfe_workspace.buckets.id
   task_id           = tfe_organization_run_task.snyk.id
   enforcement_level = "advisory"
-  stage             = "pre_apply"
+  stage             = "post_plan"
 }
 
 resource "tfe_workspace_run_task" "secrets" {
   workspace_id      = tfe_workspace.secrets.id
   task_id           = tfe_organization_run_task.snyk.id
   enforcement_level = "advisory"
-  stage             = "pre_apply"
+  stage             = "post_plan"
 }
 
 resource "tfe_workspace_run_task" "databases" {
   workspace_id      = tfe_workspace.databases.id
   task_id           = tfe_organization_run_task.snyk.id
   enforcement_level = "advisory"
-  stage             = "pre_apply"
+  stage             = "post_plan"
 }
 
 resource "tfe_workspace_run_task" "dns" {
   workspace_id      = tfe_workspace.dns.id
   task_id           = tfe_organization_run_task.snyk.id
   enforcement_level = "advisory"
-  stage             = "pre_apply"
+  stage             = "post_plan"
 }
