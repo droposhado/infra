24237ce62133f6cf7695e2321088e96ea488d7c1
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-10-09 13:41:18 -0300

    n/a

--- terraform/dns/modules.tf
+++ terraform/dns/modules.tf
@@ -1,12 +1,12 @@
+module "aws_route53" {
+  source  = "./modules/aws"
+  domain  = var.domain
+  records = local.aws_records
+}
+
 module "cloudflare" {
   source     = "./modules/cloudflare-ns"
   account_id = var.account_id
   domain     = var.domain
   ns         = module.aws_route53.ns
-}
-
-module "aws_route53" {
-  source  = "./modules/aws"
-  domain  = var.domain
-  records = local.aws_records
 }
