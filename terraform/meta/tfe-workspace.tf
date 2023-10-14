f29dee20199370ab162ca7a03c13fdebd5b77f09
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-10-13 21:12:26 -0300

    enable vcs in workspace

--- terraform/meta/tfe-workspace.tf
+++ terraform/meta/tfe-workspace.tf
@@ -75,12 +75,12 @@ resource "tfe_workspace" "storage" {
     tfe_workspace.databases.id
   ]
   terraform_version = "1.5.4"
-  # vcs_repo {
-  #   identifier                 = "${var.github_infra_user}/${var.github_infra_repo}"
-  #   branch                     = "main"
-  #   ingress_submodules         = false
-  #   github_app_installation_id = data.tfe_github_app_installation.gha_installation.id
-  # }
+  vcs_repo {
+    identifier                 = "${var.github_infra_user}/${var.github_infra_repo}"
+    branch                     = "main"
+    ingress_submodules         = false
+    github_app_installation_id = data.tfe_github_app_installation.gha_installation.id
+  }
 
   lifecycle {
     prevent_destroy = true
