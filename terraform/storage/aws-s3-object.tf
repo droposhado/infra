981797b9c9510ba8d6f899696f1114769ae68fa0
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-10-14 02:22:13 -0300

    uptes in buckets

--- terraform/storage/aws-s3-object.tf
+++ terraform/storage/aws-s3-object.tf
@@ -1,9 +1,25 @@
-data "aws_s3_object" "project_furgao_zip" {
+resource "aws_s3_object" "project_furgao_zip" {
   bucket = aws_s3_bucket.cdn.id
   key    = "projects/furgao.zip"
+  source = "${path.module}/dummy.zip"
+
+  lifecycle {
+    ignore_changes = [
+      etag,
+      source
+    ]
+  }
 }
 
-data "aws_s3_object" "project_maya_zip" {
+resource "aws_s3_object" "project_maya_zip" {
   bucket = aws_s3_bucket.cdn.id
   key    = "projects/maya.zip"
+  source = "${path.module}/dummy.zip"
+
+  lifecycle {
+    ignore_changes = [
+      etag,
+      source
+    ]
+  }
 }
