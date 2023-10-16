40542c780bdccc46761fc83fda9a636961a6e4bf
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-10-15 21:37:32 -0300

    adding subnet name to db

--- terraform/databases/modules/aws-rds/main.tf
+++ terraform/databases/modules/aws-rds/main.tf
@@ -9,6 +9,7 @@ resource "aws_db_instance" "main" {
   publicly_accessible         = var.publicly_accessible
   skip_final_snapshot         = var.skip_final_snapshot
   vpc_security_group_ids      = var.vpc_security_group_ids
+  db_subnet_group_name        = var.db_subnet_group_name
   allow_major_version_upgrade = var.allow_major_version_upgrade
   auto_minor_version_upgrade  = var.auto_minor_version_upgrade
   deletion_protection         = var.deletion_protection
