56ba8ba4a6cde4ab5ca7d55d9b45ac60da8b1559
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-10-17 16:50:52 -0300

    rename workspace k8s to compute

--- terraform/meta/tfe-workspace.tf
+++ terraform/meta/tfe-workspace.tf
@@ -136,26 +136,26 @@ resource "tfe_workspace" "secrets" {
   }
 }
 
-resource "tfe_workspace" "k8s" {
-  name              = "k8s"
+resource "tfe_workspace" "compute" {
+  name              = "compute"
   organization      = tfe_organization.main.name
   auto_apply        = true
   execution_mode    = "remote"
   force_delete      = false
   project_id        = tfe_project.compute.id
   queue_all_runs    = false
-  working_directory = "terraform/k8s"
-  trigger_patterns  = distinct(["terraform/k8s/**", "terraform/k8s/**/*"])
+  working_directory = "terraform/compute"
+  trigger_patterns  = distinct(["terraform/compute/**", "terraform/compute/**/*"])
   remote_state_consumer_ids = [
     tfe_workspace.dns.id
   ]
   terraform_version = "1.5.4"
-  vcs_repo {
-    identifier                 = "${var.github_infra_user}/${var.github_infra_repo}"
-    branch                     = "main"
-    ingress_submodules         = false
-    github_app_installation_id = data.tfe_github_app_installation.gha_installation.id
-  }
+  # vcs_repo {
+  #   identifier                 = "${var.github_infra_user}/${var.github_infra_repo}"
+  #   branch                     = "main"
+  #   ingress_submodules         = false
+  #   github_app_installation_id = data.tfe_github_app_installation.gha_installation.id
+  # }
 
   lifecycle {
     prevent_destroy = true
