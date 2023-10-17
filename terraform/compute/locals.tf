2111fc71a0a052bd08a03a213b45ee54accfcd29
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-10-17 16:55:53 -0300

    add vcs support on compute ws

--- terraform/compute/locals.tf
+++ terraform/compute/locals.tf
@@ -1,5 +1,6 @@
 locals {
   gotify    = data.terraform_remote_state.secrets.outputs.gotify
+  maya      = data.terraform_remote_state.secrets.outputs.maya
   redmine   = data.terraform_remote_state.secrets.outputs.remine
   sabedoria = data.terraform_remote_state.secrets.outputs.sabedoria
   wooka     = data.terraform_remote_state.secrets.outputs.wooka
