64d9edb6e042627c41f27fd322b2def0f9bd2b46
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-10-17 13:36:22 -0300

    now public acl

--- terraform/storage/aws-s3-bucket-acl.tf
+++ terraform/storage/aws-s3-bucket-acl.tf
@@ -2,26 +2,26 @@ resource "aws_s3_bucket_acl" "blog" {
   depends_on = [aws_s3_bucket_ownership_controls.blog]
 
   bucket = aws_s3_bucket.blog.id
-  acl    = "private"
+  acl    = "public-read"
 }
 
 resource "aws_s3_bucket_acl" "cdn" {
   depends_on = [aws_s3_bucket_ownership_controls.cdn]
 
   bucket = aws_s3_bucket.cdn.id
-  acl    = "private"
+  acl    = "public-read"
 }
 
 resource "aws_s3_bucket_acl" "maps" {
   depends_on = [aws_s3_bucket_ownership_controls.maps]
 
   bucket = aws_s3_bucket.maps.id
-  acl    = "private"
+  acl    = "public-read"
 }
 
 resource "aws_s3_bucket_acl" "wiki" {
   depends_on = [aws_s3_bucket_ownership_controls.wiki]
 
   bucket = aws_s3_bucket.wiki.id
-  acl    = "private"
+  acl    = "public-read"
 }
