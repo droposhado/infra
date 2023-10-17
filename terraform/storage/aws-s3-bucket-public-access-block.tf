60e7198ce92d321262b357b0cbabfe4cd777af25
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-10-17 13:47:42 -0300

    change acls to public access

--- terraform/storage/aws-s3-bucket-public-access-block.tf
+++ terraform/storage/aws-s3-bucket-public-access-block.tf
@@ -1,35 +1,35 @@
 resource "aws_s3_bucket_public_access_block" "blog" {
   bucket = aws_s3_bucket.blog.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_public_access_block" "cdn" {
   bucket = aws_s3_bucket.cdn.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_public_access_block" "maps" {
   bucket = aws_s3_bucket.maps.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_public_access_block" "wiki" {
   bucket = aws_s3_bucket.wiki.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
