125cb11c18c02f8c41bd383932b169cec58697fb
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-10-17 21:35:50 -0300

    add aws creds to secrets ws

--- terraform/meta/tfe-variables-secrets.tf
+++ terraform/meta/tfe-variables-secrets.tf
@@ -1,7 +1,21 @@
+resource "tfe_variable" "secrets_aws_access_key_id" {
+  key          = "AWS_ACCESS_KEY_ID"
+  value        = var.aws_access_key_id
+  category     = "env"
+  workspace_id = tfe_workspace.secrets.id
+}
+
 resource "tfe_variable" "secrets_aws_region_default" {
   key          = "aws_region_default"
   value        = var.aws_region_default
   category     = "terraform"
+  workspace_id = tfe_workspace.secrets.id
+}
+
+resource "tfe_variable" "secrets_aws_secret_access_key" {
+  key          = "AWS_SECRET_ACCESS_KEY"
+  value        = var.aws_secret_access_key
+  category     = "env"
   workspace_id = tfe_workspace.secrets.id
 }
 
