973074e1eb5f0124e114a425a5adfbe840038f4c
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-10-18 15:37:09 -0300

    add new resources to maya and redmine

--- terraform/secrets/sentry-key.tf
+++ terraform/secrets/sentry-key.tf
@@ -1,3 +1,10 @@
+resource "sentry_key" "maya" {
+  organization = sentry_organization.droposhado_org.slug
+
+  project = sentry_project.maya.slug
+  name    = "k8s-${data.terraform_remote_state.seeds.outputs.maya.name}"
+}
+
 resource "sentry_key" "sabedoria" {
   organization = sentry_organization.droposhado_org.slug
 
