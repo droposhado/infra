f65b564033cb03f5a80ef8c18886fed0fce2060c
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-10-22 02:52:17 -0300

    remove buckets on storage root

--- terraform/storage/aws-s3-bucket-acl.tf
+++ terraform/storage/aws-s3-bucket-acl.tf
@@ -1,17 +1,3 @@
-resource "aws_s3_bucket_acl" "cdn" {
-  depends_on = [aws_s3_bucket_ownership_controls.cdn]
-
-  bucket = aws_s3_bucket.cdn.id
-  acl    = "public-read"
-}
-
-resource "aws_s3_bucket_acl" "maps" {
-  depends_on = [aws_s3_bucket_ownership_controls.maps]
-
-  bucket = aws_s3_bucket.maps.id
-  acl    = "public-read"
-}
-
 resource "aws_s3_bucket_acl" "maya" {
   depends_on = [aws_s3_bucket_ownership_controls.maya]
 
@@ -24,11 +10,4 @@ resource "aws_s3_bucket_acl" "sabedoria" {
 
   bucket = aws_s3_bucket.sabedoria.id
   acl    = "private"
-}
-
-resource "aws_s3_bucket_acl" "wiki" {
-  depends_on = [aws_s3_bucket_ownership_controls.wiki]
-
-  bucket = aws_s3_bucket.wiki.id
-  acl    = "public-read"
 }
--- terraform/storage/modules.tf
+++ terraform/storage/modules.tf
@@ -1,8 +1,12 @@
 module "blog_static_site" {
-  source      = "./modules/aws-bucket"
-  acl         = "public-read"
-  bucket_name = local.blog_name
-  enable_cors = false
-  tags        = local.tags
-  versioning  = "Enabled"
+  source                  = "./modules/aws-bucket"
+  acl                     = "public-read"
+  block_public_acls       = false
+  block_public_policy     = false
+  bucket_name             = local.blog_name
+  enable_cors             = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+  tags                    = local.tags
+  versioning              = "Enabled"
 }
