edfc3c9c5f014293bf5c7ff079b634be158965d8
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-10-22 03:00:18 -0300

    remove all buckets

--- terraform/storage/outputs.tf
+++ terraform/storage/outputs.tf
@@ -2,22 +2,22 @@ output "blog" {
   value = module.blog_static_site.bucket
 }
 
-output "cdn" {
-  value = aws_s3_bucket.cdn
-}
+# output "cdn" {
+#   value = aws_s3_bucket.cdn
+# }
 
-output "maya" {
-  value = aws_s3_bucket.maya
-}
+# output "maya" {
+#   value = aws_s3_bucket.maya
+# }
 
-output "maps" {
-  value = aws_s3_bucket.maps
-}
+# output "maps" {
+#   value = aws_s3_bucket.maps
+# }
 
-output "sabedoria" {
-  value = aws_s3_bucket.sabedoria
-}
+# output "sabedoria" {
+#   value = aws_s3_bucket.sabedoria
+# }
 
-output "wiki" {
-  value = aws_s3_bucket.wiki
-}
+# output "wiki" {
+#   value = aws_s3_bucket.wiki
+# }
