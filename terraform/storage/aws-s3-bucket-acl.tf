0ac2747c9029884b9427a52641a61064df66cbcd
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-10-22 03:00:18 -0300

    remove all buckets

--- terraform/storage/aws-s3-bucket-acl.tf
+++ terraform/storage/aws-s3-bucket-acl.tf
@@ -1,13 +1,13 @@
-resource "aws_s3_bucket_acl" "maya" {
-  depends_on = [aws_s3_bucket_ownership_controls.maya]
+# resource "aws_s3_bucket_acl" "maya" {
+#   depends_on = [aws_s3_bucket_ownership_controls.maya]
 
-  bucket = aws_s3_bucket.maya.id
-  acl    = "private"
-}
+#   bucket = aws_s3_bucket.maya.id
+#   acl    = "private"
+# }
 
-resource "aws_s3_bucket_acl" "sabedoria" {
-  depends_on = [aws_s3_bucket_ownership_controls.sabedoria]
+# resource "aws_s3_bucket_acl" "sabedoria" {
+#   depends_on = [aws_s3_bucket_ownership_controls.sabedoria]
 
-  bucket = aws_s3_bucket.sabedoria.id
-  acl    = "private"
-}
+#   bucket = aws_s3_bucket.sabedoria.id
+#   acl    = "private"
+# }
--- terraform/storage/aws-s3-bucket.tf
+++ terraform/storage/aws-s3-bucket.tf
@@ -1,11 +1,11 @@
-resource "aws_s3_bucket" "maya" {
-  bucket = local.maya.name
+# resource "aws_s3_bucket" "maya" {
+#   bucket = local.maya.name
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
 
-resource "aws_s3_bucket" "sabedoria" {
-  bucket = local.sabedoria.name
+# resource "aws_s3_bucket" "sabedoria" {
+#   bucket = local.sabedoria.name
 
-  tags = local.tags
-}
+#   tags = local.tags
+# }
