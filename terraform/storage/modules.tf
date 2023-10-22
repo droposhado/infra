660e6ec9c06a18b79da9123f5e6a210c35020449
Flaverton Rodrigues Rosa <sign@droposhado.org>
2023-10-22 03:22:52 -0300

    new buckets

--- terraform/storage/modules.tf
+++ terraform/storage/modules.tf
@@ -10,3 +10,42 @@ module "blog_static_site" {
   tags                    = local.tags
   versioning              = "Enabled"
 }
+
+module "cdn_static_site" {
+  source                  = "./modules/aws-bucket"
+  acl                     = "public-read"
+  block_public_acls       = false
+  block_public_policy     = false
+  bucket_name             = local.cdn_name
+  enable_cors             = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+  tags                    = local.tags
+  versioning              = "Enabled"
+}
+
+module "maps_static_site" {
+  source                  = "./modules/aws-bucket"
+  acl                     = "public-read"
+  block_public_acls       = false
+  block_public_policy     = false
+  bucket_name             = local.maps_name
+  enable_cors             = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+  tags                    = local.tags
+  versioning              = "Enabled"
+}
+
+module "wiki_static_site" {
+  source                  = "./modules/aws-bucket"
+  acl                     = "public-read"
+  block_public_acls       = false
+  block_public_policy     = false
+  bucket_name             = local.wiki_name
+  enable_cors             = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+  tags                    = local.tags
+  versioning              = "Enabled"
+}
--- terraform/storage/outputs.tf
+++ terraform/storage/outputs.tf
@@ -2,22 +2,22 @@ output "blog" {
   value = module.blog_static_site.bucket
 }
 
-# output "cdn" {
-#   value = aws_s3_bucket.cdn
-# }
+output "cdn" {
+  value = module.cdn_static_site.bucket
+}
 
 # output "maya" {
 #   value = aws_s3_bucket.maya
 # }
 
-# output "maps" {
-#   value = aws_s3_bucket.maps
-# }
+output "maps" {
+  value = module.maps_static_site.bucket
+}
 
 # output "sabedoria" {
 #   value = aws_s3_bucket.sabedoria
 # }
 
-# output "wiki" {
-#   value = aws_s3_bucket.wiki
-# }
+output "wiki" {
+  value = module.wiki_static_site.bucket
+}
