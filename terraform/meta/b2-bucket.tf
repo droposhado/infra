8d6b78d23981cf0257c7dc10d06bb5d45c78ab5b
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-04-11 18:18:09 -0300

    adding new bucket to store backup

--- terraform/meta/b2-bucket.tf
+++ terraform/meta/b2-bucket.tf
@@ -1,3 +1,8 @@
+resource "b2_bucket" "backup" {
+  bucket_name = local.backup_bucket_name
+  bucket_type = "allPrivate"
+}
+
 resource "b2_bucket" "state" {
   bucket_name = local.state_bucket_name
   bucket_type = "allPrivate"
--- terraform/meta/github-actions-secret-backup.tf
+++ terraform/meta/github-actions-secret-backup.tf
@@ -1,23 +1,23 @@
 # resource "github_actions_secret" "aws_default_region" {
 #   repository      = var.github_infra_repo
-#   secret_name     = "AWS_DEFAULT_REGION"
+#   secret_name     = "BACKUP_AWS_DEFAULT_REGION"
 #   plaintext_value = var.b2_default_region
 # }
 
 # resource "github_actions_secret" "aws_access_key_id" {
 #   repository      = var.github_infra_repo
-#   secret_name     = "AWS_ACCESS_KEY_ID"
+#   secret_name     = "BACKUP_AWS_ACCESS_KEY_ID"
 #   plaintext_value = b2_application_key.state.application_key_id
 # }
 
 # resource "github_actions_secret" "aws_secret_access_key" {
 #   repository      = var.github_infra_repo
-#   secret_name     = "AWS_SECRET_ACCESS_KEY"
+#   secret_name     = "BACKUP_AWS_SECRET_ACCESS_KEY"
 #   plaintext_value = b2_application_key.state.application_key
 # }
 
 # resource "github_actions_secret" "sabedoria_bucket_endpoint" {
 #   repository      = var.github_infra_repo
-#   secret_name     = "AWS_ENDPOINT_URL_S3 "
+#   secret_name     = "BACKUP_AWS_ENDPOINT_URL_S3"
 #   plaintext_value = data.b2_account_info.main.s3_api_url
 # }
