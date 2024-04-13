e93ab08d25bc132a28ff2f93cfbce222f9c70777
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-04-13 15:33:24 -0300

    blog adding backend

--- terraform/blog/backend.tf
+++ terraform/blog/backend.tf
@@ -3,9 +3,8 @@ terraform {
     key    = "blog/terraform.tfstate"
 
     # required to use with b2
-    skip_requesting_account_id = true
     skip_credentials_validation = true
-    skip_get_ec2_platforms = true
-    skip_metadata_api_check = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
   }
 }
