2ff7899103c91e0660d549587b8660415229da3d
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-04-15 20:10:57 -0300

    fixing cloudflare-pages-project locals

--- terraform/modules/cloudflare-pages-project/cloudflare-record.tf
+++ terraform/modules/cloudflare-pages-project/cloudflare-record.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_record" "main" {
   zone_id = var.zone_id
-  name    = strcontains(var.subdomain, "@") ? "@" : var.subdomain
+  name    = var.subdomain
   value   = cloudflare_pages_project.main.subdomain
   type    = "CNAME"
   ttl     = 1
