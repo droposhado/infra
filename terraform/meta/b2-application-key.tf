0f86d7825e5373fc77a2b520818f384b6a2ccc60
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-04-15 11:19:49 -0300

    remove unused b2 state bucket eky

--- terraform/meta/b2-application-key.tf
+++ terraform/meta/b2-application-key.tf
@@ -8,15 +8,3 @@ resource "b2_application_key" "backup" {
     "deleteFiles"
   ]
 }
-
-resource "b2_application_key" "state" {
-  key_name     = local.state_bucket_name
-  bucket_id    = b2_bucket.state.bucket_id
-  capabilities = [
-    "listFiles",
-    "readFiles",
-    "writeFiles",
-    "deleteFiles"
-  ]
-}
-
