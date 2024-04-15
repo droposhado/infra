880dc7559e1d9c4cd2f847808e7a3c1295e2f9d8
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-04-15 11:58:34 -0300

    fix fmt

--- terraform/blog/locals.tf
+++ terraform/blog/locals.tf
@@ -1,7 +1,7 @@
 locals {
   cloudflare_account_id   = data.terraform_remote_state.meta.cloudflare.account_id
   cloudflare_project_name = module.blog_project.project_name
-  cloudflare_zone_id      = data.terraform_remote_state.meta.cloudflare.zone_id 
+  cloudflare_zone_id      = data.terraform_remote_state.meta.cloudflare.zone_id
   domain                  = data.terraform_remote_state.meta.domain.text
   domain_default_ttl      = data.terraform_remote_state.meta.domain.default_ttl
   github_blog_repo        = data.terraform_remote_state.meta.github.blog
