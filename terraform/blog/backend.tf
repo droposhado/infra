438e63c83b9d10880b46cd23a86fcad98f5e0dba
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-04-15 11:24:26 -0300

    adding new backend state to blog

--- terraform/blog/backend.tf
+++ terraform/blog/backend.tf
@@ -1,10 +1,5 @@
 terraform {
-  backend "s3" {
+  backend "gcs" {
     key = "blog/terraform.tfstate"
-
-    # required to use with b2
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
   }
 }
