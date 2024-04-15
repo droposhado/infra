bc6e77e4c18938b8296080367a4fd37296f7ea41
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-04-15 19:46:00 -0300

    remove interpolation in news ws

--- terraform/news/modules.tf
+++ terraform/news/modules.tf
@@ -1,7 +1,7 @@
 module "news_project" {
   source     = "../modules/cloudflare-pages-project"
   account_id = local.cloudflare_account_id
-  domain     = "${local.domain}"
+  domain     = local.domain
   subdomain  = "news"
   zone_id    = local.cloudflare_zone_id
 }
