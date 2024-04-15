3619611902a673108e5bfc091240470d022f3136
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-04-15 14:30:59 -0300

    remove duplicated record from blog ws

--- terraform/blog/cloudflare-record.tf
+++
@@ -1,8 +0,0 @@
-resource "cloudflare_record" "blog" {
-  zone_id = local.cloudflare_zone_id
-  name    = "@"
-  value   = local.domain
-  type    = "CNAME"
-  ttl     = local.domain_default_ttl
-  proxied = false
-}
