f53ef4fdb4ced9cc2f1eb628b233543ea73b3465
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-04-15 14:28:26 -0300

    remove unused action secret on meta ws

--- terraform/meta/github-actions-secret-infra.tf
+++ terraform/meta/github-actions-secret-infra.tf
@@ -3,9 +3,3 @@ resource "github_actions_secret" "infra_cloudflare_api_token" {
   secret_name     = "CLOUDFLARE_API_TOKEN"
   plaintext_value = var.cloudflare_api_token
 }
-
-resource "github_actions_secret" "infra_github_token" {
-  repository      = var.github_infra_repo
-  secret_name     = "TF_VAR_github_token"
-  plaintext_value = var.github_token
-}
