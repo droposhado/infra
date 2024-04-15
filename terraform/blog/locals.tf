7045d85bc2929ab15396157f0c2d9077a23f3d82
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-04-15 14:33:41 -0300

    remove unused ttl var from blog ws

--- terraform/blog/locals.tf
+++ terraform/blog/locals.tf
@@ -4,7 +4,6 @@ locals {
   cloudflare_project_name = module.blog_project.project_name
   cloudflare_zone_id      = data.terraform_remote_state.meta.outputs.cloudflare.zone_id
   domain                  = data.terraform_remote_state.meta.outputs.domain.text
-  domain_default_ttl      = data.terraform_remote_state.meta.outputs.domain.default_ttl
   github_blog_repo        = data.terraform_remote_state.meta.outputs.github.blog
   github_token            = data.terraform_remote_state.meta.outputs.github.token
 }
