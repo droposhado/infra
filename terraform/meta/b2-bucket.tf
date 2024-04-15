818f262102d96d030ee86c5962759a16baaddedd
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-04-15 11:15:01 -0300

    adding support to gcs as backend

--- terraform/meta/b2-bucket.tf
+++ terraform/meta/b2-bucket.tf
@@ -2,8 +2,3 @@ resource "b2_bucket" "backup" {
   bucket_name = local.backup_bucket_name
   bucket_type = "allPrivate"
 }
-
-resource "b2_bucket" "state" {
-  bucket_name = local.state_bucket_name
-  bucket_type = "allPrivate"
-}
--- terraform/meta/github-actions-secret-state.tf
+++ terraform/meta/github-actions-secret-state.tf
@@ -3,3 +3,9 @@ resource "github_actions_secret" "state_google_credentials" {
   secret_name     = "STATE_GOOGLE_CREDENTIALS"
   plaintext_value = var.gcp_google_credentials
 }
+
+resource "github_actions_secret" "state_gcs_bucket" {
+  repository      = var.github_infra_repo
+  secret_name     = "STATE_GCS_BUCKET"
+  plaintext_value = google_storage_bucket.state.name
+}
