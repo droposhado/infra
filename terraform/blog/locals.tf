7a5394a272860763ed4a869158931fddf06c3f2f
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-04-15 13:10:24 -0300

    blog workspace take via data remote cloudflare api

--- terraform/blog/locals.tf
+++ terraform/blog/locals.tf
@@ -1,5 +1,6 @@
 locals {
   cloudflare_account_id   = data.terraform_remote_state.meta.outputs.cloudflare.account_id
+  cloudflare_api_token    = data.terraform_remote_state.meta.outputs.cloudflare.api_token
   cloudflare_project_name = module.blog_project.project_name
   cloudflare_zone_id      = data.terraform_remote_state.meta.outputs.cloudflare.zone_id
   domain                  = data.terraform_remote_state.meta.outputs.domain.text
