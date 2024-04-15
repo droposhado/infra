b5b2d4b33c523b8ce9f0098a233436c1c218a104
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-04-15 20:07:33 -0300

    cloudflare-pages-project subdomain record to empty

--- terraform/modules/cloudflare-pages-project/cloudflare-record.tf
+++ terraform/modules/cloudflare-pages-project/cloudflare-record.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_record" "main" {
   zone_id = var.zone_id
-  name    = local.subdomain_slug
+  name    = strcontains(var.subdomain, "@") ? "@" : var.subdomain
   value   = cloudflare_pages_project.main.subdomain
   type    = "CNAME"
   ttl     = 1
