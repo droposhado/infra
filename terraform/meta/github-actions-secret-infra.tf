0f827b44d92b16bb8bd9e0771b8aa26aa28da9ca
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-04-15 12:44:27 -0300

    fix variable

--- terraform/meta/github-actions-secret-infra.tf
+++ terraform/meta/github-actions-secret-infra.tf
@@ -1,6 +1,6 @@
 resource "github_actions_secret" "infra_cloudflare_api_token" {
   repository      = var.github_infra_repo
-  secret_name     = "TF_VAR_cloudflare_api_token"
+  secret_name     = "CLOUDFLARE_API_TOKEN"
   plaintext_value = var.cloudflare_api_token
 }
 
