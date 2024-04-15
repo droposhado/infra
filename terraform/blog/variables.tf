a61553975084ba58dba534353734ef59445f0cc5
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-04-15 13:01:57 -0300

    remove unused tf cloudlfare var

--- terraform/blog/variables.tf
+++ terraform/blog/variables.tf
@@ -1,8 +1,3 @@
-variable "cloudflare_api_token" {
-  type        = string
-  description = "Token with permissions defined in workspace to deploy dns, pages"
-}
-
 variable "github_token" {
   type        = string
   description = "Token to insert vars in action and insert webhooks"
