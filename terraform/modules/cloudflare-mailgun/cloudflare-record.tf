6c7d1735442378011f6aedea0fd47f50486552ba
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-04-16 16:32:19 -0300

    adding receiving records to cloudflare-mailgun module

--- terraform/modules/cloudflare-mailgun/cloudflare-record.tf
+++ terraform/modules/cloudflare-mailgun/cloudflare-record.tf
@@ -1,5 +1,4 @@
-resource "cloudflare_record" "smtp" {
-
+resource "cloudflare_record" "sending" {
   for_each = {
     for record in mailgun_domain.main.sending_records_set : record.id => {
       type  = record.record_type
@@ -11,6 +10,25 @@ resource "cloudflare_record" "smtp" {
   type    = each.value.type
   name    = each.value.name
   value   = each.value.value
+  zone_id = var.cloudflare_zone_id
+  proxied = false
+
+  depends_on = [mailgun_domain.main]
+}
+
+resource "cloudflare_record" "receiving" {
+  for_each = {
+    for record in mailgun_domain.main.receiving_records_set : record.id => {
+      type      = record.record_type
+      priority  = record.priority
+      value     = record.value
+    }
+  }
+
+  type     = each.value.type
+  name     = "${var.subdomain}.${var.domain}"
+  value    = each.value.value
+  priority = each.value.priority
   zone_id = var.cloudflare_zone_id
   proxied = false
 
