fc05965af9257425139776ac8db58046f950f6f1
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-04-20 14:43:14 -0300

    outputs update on meta ws

--- terraform/meta/github-actions-secret-infra.tf
+++
@@ -1,5 +0,0 @@
-resource "github_actions_secret" "infra_cloudflare_api_token" {
-  repository      = var.github_infra_repo
-  secret_name     = "CLOUDFLARE_API_TOKEN"
-  plaintext_value = var.cloudflare_api_token
-}
