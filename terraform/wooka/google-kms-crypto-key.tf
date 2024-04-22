7b6ac27dc2c2a7e8a8faa3df2fd79bf56fa3f3ba
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-04-22 10:50:38 -0300

    wooka ws updates with bootdisk encrypted

--- terraform/wooka/google-kms-crypto-key.tf
+++ terraform/wooka/google-kms-crypto-key.tf
@@ -1,8 +1,9 @@
 resource "google_kms_crypto_key" "main" {
-  name     = "crypto-key-example"
-  key_ring = google_kms_key_ring.main.id
+  name            = "crypto-key-example"
+  key_ring        = google_kms_key_ring.main.id
+  rotation_period = "7776000s"
 
   lifecycle {
-    prevent_destroy = false
+    prevent_destroy = true
   }
 }
