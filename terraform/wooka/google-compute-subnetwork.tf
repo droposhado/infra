3e675afaaac18d29b11b88df00bd905bed90eb2b
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-04-22 09:37:03 -0300

    wooka ws updates with security in vm and network

--- terraform/wooka/google-compute-subnetwork.tf
+++ terraform/wooka/google-compute-subnetwork.tf
@@ -1,5 +1,11 @@
 resource "google_compute_subnetwork" "main" {
-  name          = module.subnetwork_name.name
-  ip_cidr_range = "10.0.1.0/24"
-  network       = google_compute_network.main.self_link
+  name                     = module.subnetwork_name.name
+  ip_cidr_range            = "10.0.1.0/24"
+  network                  = google_compute_network.main.self_link
+  private_ip_google_access = true
+
+  log_config {
+    aggregation_interval = "INTERVAL_5_SEC"
+    flow_sampling        = 0.5
+  }
 }
