048e9423c26f56fcf07df48aabae122a59bcbcd5
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-04-22 10:14:03 -0300

    wooka ws updates with security in vm and network

--- terraform/wooka/google-compute-network.tf
+++ terraform/wooka/google-compute-network.tf
@@ -1,5 +1,6 @@
 resource "google_compute_network" "main" {
-  name                    = module.network_name.name
-  auto_create_subnetworks = false
-  routing_mode            = "REGIONAL"
+  name                     = module.network_name.name
+  auto_create_subnetworks  = false
+  enable_ula_internal_ipv6 = true
+  routing_mode             = "REGIONAL"
 }
--- terraform/wooka/google-compute-subnetwork.tf
+++ terraform/wooka/google-compute-subnetwork.tf
@@ -5,6 +5,9 @@ resource "google_compute_subnetwork" "main" {
   private_ip_google_access   = true
   private_ipv6_google_access = true
 
+  stack_type       = "IPV4_IPV6"
+  ipv6_access_type = "INTERNAL"
+
   log_config {
     aggregation_interval = "INTERVAL_5_SEC"
     flow_sampling        = 0.5
