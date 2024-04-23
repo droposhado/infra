801104175c5301878ece691ed49a4904a80b3b0a
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-04-23 10:49:55 -0300

    starting migration from hcp cloud to gcs state

--- terraform/meta/google-storage-bucket.tf
+++
@@ -1,9 +0,0 @@
-resource "google_storage_bucket" "state" {
-  name          = local.state_bucket_name
-  location      = var.gcp_region
-  storage_class = "STANDARD"
-
-  versioning {
-    enabled = true
-  }
-}
