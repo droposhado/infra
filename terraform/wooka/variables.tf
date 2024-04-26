b8693ebd3fe5c6d683e1afc1ee27904d54f1dd75
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-04-26 19:51:40 -0300

    wooka updates

---
+++ terraform/wooka/variables.tf
@@ -0,0 +1,19 @@
+variable "wooka_bot_admins" {
+  type = string
+}
+
+variable "wooka_bot_alt_prefixes" {
+  type = string
+}
+
+variable "wooka_zulip_email" {
+  type = string
+}
+
+variable "wooka_zulip_key" {
+  type = string
+}
+
+variable "wooka_zulip_site" {
+  type = string
+}
