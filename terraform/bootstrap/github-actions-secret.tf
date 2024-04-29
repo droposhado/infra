3f7ef6e999d3494e6e53a82aab6681bc61398fb4
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-04-29 20:46:06 -0300

    updating to use previusly generated ssh key

--- terraform/bootstrap/github-actions-secret.tf
+++ terraform/bootstrap/github-actions-secret.tf
@@ -1,5 +1,5 @@
 resource "github_actions_secret" "infra_private" {
   repository      = var.github_infra_repo
   secret_name     = "INFRA_MIRROR_SSH_KEY"
-  encrypted_value = base64encode(tls_private_key.mirror.private_key_openssh)
+  encrypted_value = var.infra_ssh_private
 }
