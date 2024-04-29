6e4ca7bd24918d0de45c5d82c3af89c2567187fe
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-04-29 20:46:06 -0300

    updating to use previusly generated ssh key

--- terraform/bootstrap/github-repository-deploy-key.tf
+++ terraform/bootstrap/github-repository-deploy-key.tf
@@ -1,6 +1,6 @@
 resource "github_repository_deploy_key" "mirror" {
   title      = "key to mirror infra private repository to public"
   repository = var.github_infra_public_repo
-  key        = tls_private_key.mirror.public_key_openssh
+  key        = var.infra_ssh_public
   read_only  = false
 }
