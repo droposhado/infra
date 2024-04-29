f243dd27c20b636eb08b7e8824aef95fa3a41017
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-04-29 18:30:26 -0300

    remove unused cloudflare secret from gh actions

--- terraform/bootstrap/github-actions-secret.tf
+++ terraform/bootstrap/github-actions-secret.tf
@@ -1,11 +1,5 @@
 resource "github_actions_secret" "infra_private" {
   repository      = var.github_infra_repo
-  secret_name     = "CLOUDFLARE_ACCOUNT_ID"
-  plaintext_value = data.cloudflare_zone.main.account_id
-}
-
-resource "github_actions_secret" "infra_public" {
-  repository      = var.github_infra_public_repo
-  secret_name     = "CLOUDFLARE_ACCOUNT_ID"
-  plaintext_value = data.cloudflare_zone.main.account_id
+  secret_name     = "INFRA_MIRROR_SSH_KEY"
+  plaintext_value = tls_private_key.mirror.private_key_openssh
 }
