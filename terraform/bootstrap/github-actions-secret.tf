3d4716b9efbfbe6290d70794fecbd71d4ab94f04
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-04-30 06:20:39 -0300

    CKV_GIT_4 skip github actions secret encrypt to pass plaintext
    bypass to private key as plaintext on github actions secret

--- terraform/bootstrap/github-actions-secret.tf
+++ terraform/bootstrap/github-actions-secret.tf
@@ -1,5 +1,7 @@
 resource "github_actions_secret" "infra_private" {
-  repository      = var.github_infra_repo
-  secret_name     = "INFRA_MIRROR_SSH_KEY"
-  encrypted_value = var.infra_ssh_private
+  repository  = var.github_infra_repo
+  secret_name = "INFRA_MIRROR_SSH_KEY"
+
+  #checkov:skip=CKV_GIT_4:This content not need be encrypted
+  plaintext_value = tls_private_key.mirror.private_key_openssh
 }
