787cd3e67bf98beef5543e9656dbd08da5c4ab95
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-05-01 17:29:22 -0300

    update gh actions on blog and lp workspaces

--- terraform/blog/github-actions-secret.tf
+++ terraform/blog/github-actions-secret.tf
@@ -5,9 +5,12 @@ resource "github_actions_secret" "cloudflare_account_id" {
 }
 
 resource "github_actions_secret" "cloudflare_api_token" {
-  repository      = var.github_blog_repo
-  secret_name     = "CLOUDFLARE_API_TOKEN"
-  encrypted_value = base64encode(local.cloudflare_project_token)
+  repository  = var.github_blog_repo
+  secret_name = "CLOUDFLARE_API_TOKEN"
+
+  # Works fine, because encripted pass base64 value as final and broke build
+  #checkov:skip=CKV_GIT_4:This content not need be encrypted
+  plaintext_value = local.cloudflare_project_token
 }
 
 resource "github_actions_secret" "cloudflare_project_name" {
