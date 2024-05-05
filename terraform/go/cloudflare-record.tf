bef52b0e3f4b6b6ceb185a9e4b33755ab4278ab0
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-05-05 19:58:52 -0300

    makefile updates

--- terraform/go/cloudflare-record.tf
+++ terraform/go/cloudflare-record.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_record" "main" {
   zone_id = data.cloudflare_zone.main.id
-  name    = "go"
+  name    = var.subdomain
   value   = "ghs.googlehosted.com"
   type    = "CNAME"
   ttl     = 3600
