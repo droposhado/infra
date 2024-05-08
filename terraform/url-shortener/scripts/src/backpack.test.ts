9e26c5c2781ab8510c93585cab355c8efb9c607f
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-05-08 10:33:42 -0300

    url-shortener backpack tests, use 1 instance

--- terraform/url-shortener/scripts/src/backpack.test.ts
+++ terraform/url-shortener/scripts/src/backpack.test.ts
@@ -9,43 +9,37 @@ const MOCK_ENV = {
 }
 
 describe('Backpack', () => {
+  const backpack = new Backpack
   it('Random string', async () => {
-    const backpack = new Backpack
     const randomString = backpack.createRandomString(chars, MOCK_ENV.SLUG_SIZE)
     expect(randomString.length).toBe(5)
   })
   it('Valid http URL', async () => {
-    const backpack = new Backpack
     const url = "http://example.com"
     const result = backpack.isValidURL(url)
     expect(result).toBe(true)
   })
   it('Valid https URL', async () => {
-    const backpack = new Backpack
     const url = "https://example.com"
     const result = backpack.isValidURL(url)
     expect(result).toBe(true)
   })
   it('Invalid http URL', async () => {
-    const backpack = new Backpack
     const url = "http://examplecom"
     const result = backpack.isValidURL(url)
     expect(result).toBe(false)
   })
   it('Invalid https URL', async () => {
-    const backpack = new Backpack
     const url = "https://examplecom"
     const result = backpack.isValidURL(url)
     expect(result).toBe(false)
   })
   it('Invalid schema', async () => {
-    const backpack = new Backpack
     const url = "postgres://user:pass@examplecom:5432/database"
     const result = backpack.isValidURL(url)
     expect(result).toBe(false)
   })
   it('Schema typo', async () => {
-    const backpack = new Backpack
     const url = "htpps://example.com"
     const result = backpack.isValidURL(url)
     expect(result).toBe(false)
