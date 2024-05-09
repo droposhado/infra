1613ee009c43cbb34c8c60fff7e3abef62fa1a8a
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-05-09 13:54:16 -0300

    blog use environment variable domain

--- terraform/blog/locals.tf
+++ terraform/blog/locals.tf
@@ -1,5 +1,4 @@
 locals {
   cloudflare_project_name  = module.blog_project.project_name
   cloudflare_project_token = module.blog_deploy_token.token
-  domain                   = data.terraform_remote_state.bootstrap.outputs.domain.text
 }
--- terraform/blog/terraform-remote-state.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "bootstrap" {
-  backend = "gcs"
-
-  config = {
-    bucket = var.gcs_bucket
-    prefix = "bootstrap"
-  }
-}
