0e525c59a58b3a621694b09b589e4dcbc4fa14eb
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-05-09 13:53:41 -0300

    lp and maps workspaces use environment variable domain

--- terraform/lp/locals.tf
+++ terraform/lp/locals.tf
@@ -1,5 +1,4 @@
 locals {
   cloudflare_project_name  = module.lp_project.project_name
   cloudflare_project_token = module.lp_deploy_token.token
-  domain                   = data.terraform_remote_state.bootstrap.outputs.domain.text
 }
