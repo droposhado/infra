834529bcf6a83604364c260a263656f11f787e90
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-05-25 19:13:51 -0300

    fixing lint and fmt

--- terraform/cdn/locals.tf
+++ terraform/cdn/locals.tf
@@ -1,5 +1,5 @@
 locals {
   access_key = cloudflare_api_token.main.id
   secret_key = sha256(cloudflare_api_token.main.value)
-  cdn_name = module.cdn_name.name
+  cdn_name   = module.cdn_name.name
 }
