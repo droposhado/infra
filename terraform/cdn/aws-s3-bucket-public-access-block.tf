94eb84cc02b6dddabcbd7b87065a21dfc65b88bd
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-05-25 22:27:45 -0300

    update provider in cdn

--- terraform/cdn/aws-s3-bucket-public-access-block.tf
+++ terraform/cdn/aws-s3-bucket-public-access-block.tf
@@ -1,4 +1,6 @@
 resource "aws_s3_bucket_public_access_block" "main" {
+  provider = aws.cloudflare
+
   bucket = aws_s3_bucket.main.id
 
   block_public_acls       = true
--- terraform/cdn/aws-s3-bucket-versioning.tf
+++ terraform/cdn/aws-s3-bucket-versioning.tf
@@ -1,4 +1,6 @@
 resource "aws_s3_bucket_versioning" "main" {
+  provider = aws.cloudflare
+
   bucket = aws_s3_bucket.main.id
   versioning_configuration {
     status = "Enabled"
