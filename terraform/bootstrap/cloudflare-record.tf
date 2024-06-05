0d3b3f4e158cb2d55901436d3433efac62342531
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-06-05 07:56:25 -0300

    custom package domain remove pypi and deb, add npm

--- terraform/bootstrap/cloudflare-record.tf
+++ terraform/bootstrap/cloudflare-record.tf
@@ -1,16 +1,7 @@
 resource "cloudflare_record" "deb" {
   zone_id = data.cloudflare_zone.main.id
-  name    = "deb"
-  value   = "deb.${var.domain}.furyns.com"
-  type    = "CNAME"
-  ttl     = var.default_ttl
-  proxied = false
-}
-
-resource "cloudflare_record" "pypi" {
-  zone_id = data.cloudflare_zone.main.id
-  name    = "pypi"
-  value   = "pypi.${var.domain}.furyns.com"
+  name    = "npm.pkg"
+  value   = "npm.pkg.${var.domain}.furyns.com"
   type    = "CNAME"
   ttl     = var.default_ttl
   proxied = false
