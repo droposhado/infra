60608183b3ff4a92d7f1fbb4acf97617c3f9859f
Flaverton Rodrigues Rosa <sign@droposhado.org>
2024-10-30 17:11:31 -0300

    url-shortener: route adjustment

--- terraform/url-shortener/cloudflare-worker-route.tf
+++ terraform/url-shortener/cloudflare-worker-route.tf
@@ -1,5 +1,5 @@
 resource "cloudflare_worker_route" "root" {
   zone_id     = data.cloudflare_zone.main.id
-  pattern     = "${local.fqdn}/"
+  pattern     = "${local.fqdn}/*"
   script_name = cloudflare_worker_script.root.name
 }
