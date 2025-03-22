1437abe5e76dd33ce7af26573fd3a25bbfdbc66c
Flaverton Rodrigues Rosa <sign@droposhado.org>
2025-03-22 13:13:41 -0300

    module update

--- terraform/modules/cloudflare-pages-project/cloudflare-pages-project.tf
+++ terraform/modules/cloudflare-pages-project/cloudflare-pages-project.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_pages_project" "main" {
   account_id        = data.cloudflare_zone.main.account_id
-  name              = "${local.subdomain_slug}${local.domain_slug}"
+  name              = local.fqdn_slug
   production_branch = "main"
   lifecycle {
     ignore_changes = [
